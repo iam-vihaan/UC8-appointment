456c57e3ad3144fc8269f9b2fb9cc3fdf5268096
iam-vihaan <asathyavihaan@gmail.com>
2025-06-10 17:28:16 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket       = "kasi-hcl-bucket-uc78-terraform-state-bucket"
-    key          = "kasi-hcl-bucket-uc78/statefile.tfstate"
+    bucket       = "kasi-hcl-bucket-uc8-terraform-state-bucket"
+    key          = "kasi-hcl-bucket-uc8/statefile.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
