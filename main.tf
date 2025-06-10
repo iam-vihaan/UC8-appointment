54c9eb31fe9043f6013a687f40202234d1f33853
iam-vihaan <asathyavihaan@gmail.com>
2025-06-10 15:35:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
-provider "aws" {
-  region = var.aws_region
-  access_key = var.access_key
-  secret_key = var.secret_key  
-  
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
+ }
+
+  }
+  required_version = ">= 1.10.0"
 }
 
 module "network" {
