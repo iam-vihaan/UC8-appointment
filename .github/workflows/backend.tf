21732bcfa98c0d55a821793b63bbd3f2e9ac7c22
iam-vihaan <asathyavihaan@gmail.com>
2025-06-10 17:22:14 +0530

    Delete .github/workflows/backend.tf

--- .github/workflows/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "kasi-hcl-bucket-uc78-terraform-state-bucket"
-    key          = "kasi-hcl-bucket-uc78/statefile.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
