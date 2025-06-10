0e5ed3814ef04612ff006897846cbdb545991199
iam-vihaan <asathyavihaan@gmail.com>
2025-06-10 14:23:46 +0530

    Update variable.tf

--- modules/network/variable.tf
+++ modules/network/variable.tf
@@ -1,1 +1,23 @@
+variable "aws_vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
 
+variable "aws_public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "aws_private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability zone for the subnets"
+  type        = string
+  default     = "us-east-1a"
+}
