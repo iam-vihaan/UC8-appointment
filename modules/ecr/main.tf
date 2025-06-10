51ec150ea1974897d1ff54365d88ac182a02034e
iam-vihaan <asathyavihaan@gmail.com>
2025-06-10 14:35:50 +0530

    Update main.tf

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,5 +1,5 @@
-resource "aws_ecr_repository" "kasi_hcl_repo" {
-  name                 = "kasi_hcl-repo"
+resource "aws_ecr_repository" "uc8-bucket-ecr" {
+  name                 = "uc8-bucket-ecr"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
