d27b0d950b3c3d9de26a3a071674a3decddcb9c0
iam-vihaan <asathyavihaan@gmail.com>
2025-06-10 15:59:53 +0530

    Update main.tf

--- modules/S3/main.tf
+++ modules/S3/main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "kasi_hcl_bucket" {
+resource "aws_s3_bucket" "kasi_hcl_bucket_uc8" {
   bucket = var.aws_s3_bucket_name
   object_lock_enabled = var.object_lock_enabled
   tags = var.tags
